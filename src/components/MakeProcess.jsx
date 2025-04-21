66e1678da696ab541b4a047fd61c1945d40afd41
Cristian Lopez <cristian.lopez5@utp.edu.co>
2025-04-21 08:10:36 -0500

    Update MakeProcess.jsx

--- src/components/MakeProcess.jsx
+++ src/components/MakeProcess.jsx
@@ -5,7 +5,7 @@ export function MakeProcess({ algorithm, sequence, setFlag }) {
     <div className="flex flex-col items-start justify-items-start p-4">
       <div className="bg-white shadow-md rounded-lg p-6 w-1/2">
         <h3 className="text-xl font-semibold mb-4">{algorithm}</h3>
-        <p className="text-lg">{sequence.join(" ")}</p>
+        <p className="text-lg w-full">{sequence.join(" ")}</p>
       </div>
     </div>
   );
